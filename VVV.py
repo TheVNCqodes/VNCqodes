71f26550406f2cfa3661901a2098faf6b5b10eff
Codepool <casuallycoreen@gmail.com>
2025-05-21 21:28:37 +0800

    Add files via upload

---
+++ VVV.py
@@ -0,0 +1,7 @@
+name = input ("What is your name?: ")
+message = input ("I miss you")
+duration = int = 12
+
+print(f"Hello {name}!")
+print("Happy Anniversary!")
+print(f"We are {duration} months in!")
